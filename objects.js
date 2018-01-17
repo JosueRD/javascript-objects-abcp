34bcb37cfcff51cc7381752762a5a1e5cf8a9c89
Josue Rios <josuerd89@gmail.com>
2018-01-17 20:53:56 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -2,12 +2,10 @@ var playlist
 playlist={bob:"sing1"};
 
 function updatePlaylist(playlist, artistName,songTitle){
-  var playlist
   playlist[artistName]=songTitle
   return playlist
 }
 
 function removeFromPlaylist(playlist,set){
-var playlist
   delete playlist[set];
 return playlist}
